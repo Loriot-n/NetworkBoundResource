3656990602c4272b6967561d26b2310b0acc9778
Nicolas Loriot <nicolas@loriot.xyz>
2019-03-22 11:01:56 -0700

    Add related classes used by NetworkBoundResource

---
+++ AppExecutors.kt
@@ -0,0 +1,39 @@
+/**
+ * Global executor pools for the whole application.
+ *
+ * Grouping tasks like this avoids the effects of task starvation (e.g. disk reads don't wait behind
+ * webservice requests).
+ */
+@Singleton
+open class AppExecutors(
+    private val diskIO: Executor,
+    private val networkIO: Executor,
+    private val mainThread: Executor
+) {
+
+    @Inject
+    constructor() : this(
+        Executors.newSingleThreadExecutor(),
+        Executors.newFixedThreadPool(3),
+        MainThreadExecutor()
+    )
+
+    fun diskIO(): Executor {
+        return diskIO
+    }
+
+    fun networkIO(): Executor {
+        return networkIO
+    }
+
+    fun mainThread(): Executor {
+        return mainThread
+    }
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+}
